968559acb2b1ca08efd4588b951498f9e62e909c
Anthony Piñeyro <apjoke23@gmail.com>
2024-05-30 20:34:27 -0300

    Category.jsx Maquetado

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,143 @@
-import React from 'react'
+import React from "react";
 
 export default function Categories() {
   return (
-    <div></div>
-  )
+    <div className="container border border-primary">
+      <div className="row border border-danger pt-3 pb-3">
+        <div className="col-3 border border-dark ">Option 1</div>
+        <div className="col-3 border border-dark">Option 2</div>
+        <div className="col-3 border border-dark">Optione 3</div>
+        <div className="col-3 border border-dark">Optione 4</div>
+      </div>
+      <div className="mt-3 mb-3">Categorye Name </div>
+      <div className="row border border-danger pt-3 pb-3">
+        <div className="col-3 border border-dark">Option 1</div>
+        <div className="col-3 border border-dark">Optione 2</div>
+        <div className="col-3 border border-dark">Optione 3</div>
+        <div className="col-3 border border-dark">Option 4</div>
+      </div>
+      <div className="row border border-danger pt-3 pb-3 ">
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card border border-dark">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
