03f9ad0944af805e7e4219c2f429436093c1bc8d
Demian Brinville Piquinela <demian111101@gmail.com>
2024-05-30 18:52:49 -0300

    base proyect

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import User from "./pages/User";
+import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import AboutThisProyect from "./pages/AboutThisProyect";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/product",
+      element: <Product />,
+    },
+    {
+      path: "/user",
+      element: <User />,
+    },
+    {
+      path: "/category",
+      element: <Category />,
+    },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+    {
+      path: "/about-this-proyect",
+      element: <AboutThisProyect />,
+    },
+    {
+      path: "*",
+      element: <div>Error 404</div>,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/AboutThisProyect.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutThisProyect() {
+  return (
+    <div>AboutThisProyect</div>
+  )
+}
