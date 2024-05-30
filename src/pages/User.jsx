214acfc822b2091b3fd085dd4d8b163be9b3a714
alhena1125 <alhena1125@gmail.com>
2024-05-30 20:59:30 -0300

    user

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function User() {
   return (
-    <div>User</div>
-  )
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <div>Foto</div>
+            <div>Apartados sidebar</div>
+          </div>
+
+          <div className="col-10">
+            <div>nombre</div>
+            <div className="row">
+              <div className="col-6">hola</div>
+              <div className="col-6">hola</div>
+              <div className="col-6">hola</div>
+              <div className="col-6">hola</div>
+              <div className="col-6">hola</div>
+              <div className="col-6">hola</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
