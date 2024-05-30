0614821615abcb2e8d2a5b1f226b2d45b82b33b8
Anthony Piñeyro <apjoke23@gmail.com>
2024-05-30 20:52:06 -0300

    Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function Product() {
   return (
-    <div>Product</div>
-  )
+    <div className="container border border-primary">
+      <div className="row border border-danger pt-3 pb-3 mt-3 mb-3">
+        <div className="col-8 border border-dark ">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 border border-dark">
+          <div>
+            <p>Colors</p>
+            <div className="d-flex">
+              <div className="col-3 border border-dark">R</div>
+              <div className="col-3 border border-dark">G</div>
+              <div className="col-3 border border-dark">B</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 border border-dark"></div>
+      </div>
+    </div>
+  );
 }
