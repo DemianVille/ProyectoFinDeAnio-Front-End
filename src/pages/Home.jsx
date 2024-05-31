f2efebff5ce2d58c5b190b2bc35a85cae4fb532c
Anthony Piñeyro <apjoke23@gmail.com>
2024-05-31 20:41:07 -0300

    Home.jsx Maquetado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,222 @@
-import React from 'react'
-import NavBarHome from '../components/NavBarHome'
+import React from "react";
+import NavBarHome from "../components/NavBarHome";
 
 export default function Home() {
   return (
     <>
-    <NavBarHome/>
+      <NavBarHome />
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-center">
+            <h1>Header</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div id="carouselExampleCaptions" class="carousel slide altura">
+            <div class="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="..." class="d-block w-100" alt="..." />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>
+                    Some representative placeholder content for the first slide.
+                  </p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="..." />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>
+                    Some representative placeholder content for the second
+                    slide.
+                  </p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="..." />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>
+                    Some representative placeholder content for the third slide.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div>
+            <h2>Imagen</h2>
+            <h2>Promociones</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <ul>
+              {" "}
+              <p>Categorias</p>
+              <li>Hombre</li>
+              <li>Mujer</li>
+              <li>Niño</li>
+              <li>Calzado</li>
+              <li>Merchandaising</li>
+            </ul>
+            <ul>
+              <p>Filtros</p>
+              <li>Precio</li>
+              <li>Talle</li>
+              <li>Ofertas</li>
+            </ul>
+          </div>
+          <div className="col-9">
+            <div className="row">
+              <div className="col-4">
+                {" "}
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                {" "}
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                {" "}
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                {" "}
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                {" "}
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                {" "}
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
