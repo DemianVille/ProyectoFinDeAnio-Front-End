853407967f8d005683daf4cdb8e3a3993285ba55
Anthony Piñeyro <anthony.pineyro@anima.edu.uy>
2024-06-06 21:01:17 -0300

    categories.jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,8 +5,8 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 export default function Categories() {
+  const [categories, setCategories] = useState([]);
   const [category, setCategory] = useState({});
-  const [products, setProducts] = useState([]);
   const params = useParams();
   const [filterProducts, setFilterProducts] = useState([]);
 
@@ -33,6 +33,31 @@ export default function Categories() {
       }
     };
     getCategory();
+  }, []);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            /*      Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOGEzZDhjOGZlMjRlNzlkMmJjN2IyZjYyMmRlMDU2MyIsInN1YiI6IjY2NDUzYmFhYTE3ZjJiYzVkNjJkNzc1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C6hY6n2PKJhRMxLnv2n0Fp57fvRLTtX3bsEW_ipnANE", */
+          },
+        };
+
+        const response = await fetch(
+          `http://localhost:3000/categories`,
+          options
+        );
+        const allCategoriesObject = await response.json();
+        setCategories(allCategoriesObject);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getCategories();
   }, []);
 
   useEffect(() => {
@@ -64,20 +89,34 @@ export default function Categories() {
   return (
     <>
       <NavBar />
-      <div className="container body border border-primary">
-        <div className="row border border-danger pt-3 pb-3">
-          <div className="col-3 border border-dark ">Option 1</div>
-          <div className="col-3 border border-dark">Option 2</div>
-          <div className="col-3 border border-dark">Optione 3</div>
-          <div className="col-3 border border-dark">Optione 4</div>
+      <div className="container body ">
+        <div className="row pt-3 pb-3">
+          <h4 className="p-0 col-4 d-flex justify-content-evenly">
+            {categories.map((category) => {
+              return (
+                <button className="categoryItem">
+                  <Link
+                    key={category.id}
+                    className="linkLi"
+                    to={`/category/${category.id}`}
+                  >
+                    <p className="w-100 m-0 filterStyle">{category.name}</p>
+                  </Link>
+                </button>
+              );
+            })}
+            <button className="categoryItem   ">Precio</button>
+            <button className="categoryItem  ">Talle</button>
+            <button className="categoryItem  ">Ofertas</button>
+          </h4>
+          {/*  <h4 className="unstyleList p-0  col-4 d-flex justify-content-evenly">
+            <p className="filterStyle firstItem">Precio</p>
+            <p className="filterStyle ">Talle</p>
+            <p className="filterStyle lastItem">Ofertas</p>
+          </h4> */}
         </div>
-        <h2 className="mt-3 mb-3 text-center">{category.name}</h2>
-        <div className="row border border-danger pt-3 pb-3">
-          <div className="col-3 border border-dark">Option 1</div>
-          <div className="col-3 border border-dark">Optione 2</div>
-          <div className="col-3 border border-dark">Optione 3</div>
-          <div className="col-3 border border-dark">Option 4</div>
-        </div>
+        <h3 className="mt-3 mb-3 text-center">{category.name}</h3>
+
         <div className="row border border-danger pt-3 pb-3 ">
           {filterProducts.map((product) => {
             return (
