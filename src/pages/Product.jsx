70b3def10ae86870e7ee5d212707e2995277649b
Demian Brinville Piquinela <demian111101@gmail.com>
2024-06-11 20:52:28 -0300

    product update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,7 +5,18 @@ import NavBar from "../components/NavBar";
 
 export default function Product() {
   const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
   const params = useParams();
+
+  const addQty = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const resQty = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
   useEffect(() => {
     const getProduct = async () => {
@@ -38,25 +49,57 @@ export default function Product() {
       <div className="container body">
         <div className="row pt-3 pb-3 mt-3 mb-3">
           <div className="col-8 ">
-            <div className="card h-100">
+            <div className="w-100">
               <img src={product.photo} className="card-img h-100" />
             </div>
           </div>
-          <div className="col-4 border border-dark">
-            <h2>{product.name}</h2>
+          <div className="col-4">
+            <h2 className="mb-3">{product.name}</h2>
+            <p>${product.price}.00</p>
+            <p>{product.description}</p>
+            <hr />
             <div>
-              <p>Colors</p>
+              <p>Colors -</p>
               <div className="d-flex">
-                <div className="col-3 border border-dark">R</div>
-                <div className="col-3 border border-dark">G</div>
-                <div className="col-3 border border-dark">B</div>
+                <div className="col-3 colorStyle">
+                  <div className="color1"></div>
+                </div>
+                <div className="col-3 colorStyle">
+                  <div className="color2"></div>
+                </div>
+                <div className="col-3 colorStyle">
+                  <div className="color3"></div>
+                </div>
               </div>
             </div>
+            <p className="mt-3">Cantidad -</p>
+            <div>
+              <div className="d-flex qtyDiv">
+                <button
+                  className="qtyBtn"
+                  onClick={() => {
+                    resQty();
+                  }}
+                >
+                  -
+                </button>
+                <p className="m-0 qtyNum">{quantity}</p>
+                <button
+                  className="qtyBtn"
+                  onClick={() => {
+                    addQty();
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <button className="mt-4 w-100 addToCart">
+              <b>Añadir al carro</b>
+            </button>
           </div>
         </div>
-        <div className="row">
-          <div className="col-12 border border-dark">{product.description}</div>
-        </div>
+        <div className="row"></div>
       </div>
       <Footer />
     </>
