daeee7834f92928a50e7886b05758518e43fa67a
Sofía Rovati <sofia.b.rovati.f@gmail.com>
2024-06-17 21:04:28 -0300

    'bout the proyect

--- src/pages/AboutThisProyect.jsx
+++ src/pages/AboutThisProyect.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Button, Image, Alert } from "react-bootstrap";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function AboutThisProyect() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <>
       <NavBar />
       <Container className="body">
         <header>
           <Row>
-            <Col md={8}>
-              <h5></h5>
+            <Col md={12} className="aboutProyect d-flex">
+              <div>
+                <h2>Sobre este proyecto</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Veniam, architecto reiciendis! Deserunt veniam odit dolorem
+                  quo, id non velit magnam debitis, vel porro fuga modi,
+                  consequatur reprehenderit quisquam perspiciatis iusto.
+                </p>
+              </div>
+              <div>
+                <div className="imgHomeBefore">
+                  <Image
+                    className="w-50"
+                    data-aos="fade-left"
+                    data-aos-anchor="#example-anchor"
+                    data-aos-offset="500"
+                    data-aos-duration="1100"
+                    src="/src/assets/homeAfter.jpg"
+                  />
+                </div>
+              </div>
             </Col>
             <Col md={4}>
               <Image src="" alt="" fluid />
