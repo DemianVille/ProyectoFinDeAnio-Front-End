f75667bd265bc4f712ec3c81071c539f37c5b5d7
Demian Brinville Piquinela <demian111101@gmail.com>
2024-06-20 21:03:38 -0300

    products map colors

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,10 +3,10 @@ import { ToastContainer, toast } from "react-toastify";
 import { useParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Product() {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState([]);
   const [quantity, setQuantity] = useState(1);
   const params = useParams();
 
@@ -48,58 +48,57 @@ export default function Product() {
   }, []);
 
   return (
-    <>
-      <NavBar />
-      <Container className="body">
-        <Row className="pt-3 pb-3 mt-3 mb-3">
-          <Col md={8}>
-            <div className="w-100">
-              <img src={product.photo} className="card-img h-100" alt={product.name} />
-            </div>
-          </Col>
-          <Col md={4}>
-            <h2 className="mb-3">{product.name}</h2>
-            <p className="fontRoboto">${product.price}.00</p>
-            <p>{product.description}</p>
-            <hr />
-            <div>
-              <p>Colors -</p>
-              <div className="d-flex">
-                <div className="colorStyle" onClick={notify}>
-                  <div className="color1"></div>
-                </div>
-                <div className="colorStyle" onClick={notify}>
-                  <div className="color2"></div>
-                </div>
-                <div className="colorStyle" onClick={notify}>
-                  <div className="color3"></div>
+    product.colors && (
+      <>
+        <NavBar />
+        <Container className="body">
+          <Row className="pt-3 pb-3 mt-3 mb-3">
+            <Col md={8}>
+              <div className="w-100">
+                <img
+                  src={product.photo}
+                  className="card-img h-100"
+                  alt={product.name}
+                />
+              </div>
+            </Col>
+            <Col md={4}>
+              <h2 className="mb-3">{product.name}</h2>
+              <p className="fontRoboto">${product.price}.00</p>
+              <p>{product.description}</p>
+              <hr />
+              <div>
+                <p>Colors -</p>
+                <div className="d-flex">
+                  {product.colors.colors.map((color) => {
+                    console.log(color);
+                    return (
+                      <div className="colorStyle" onClick={notify}>
+                        <div className={color}></div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
-            </div>
-            <p className="mt-3">Cantidad -</p>
-            <div className="d-flex qtyDiv">
-              <button
-                className="qtyBtn"
-                onClick={resQty}
-              >
-                -
+              <p className="mt-3">Cantidad -</p>
+              <div className="d-flex qtyDiv">
+                <button className="qtyBtn" onClick={resQty}>
+                  -
+                </button>
+                <p className="m-0 qtyNum fontRoboto">{quantity}</p>
+                <button className="qtyBtn" onClick={addQty}>
+                  +
+                </button>
+              </div>
+              <button className="mt-4 w-100 addToCart" onClick={notify}>
+                <b>Añadir al carro</b>
               </button>
-              <p className="m-0 qtyNum fontRoboto">{quantity}</p>
-              <button
-                className="qtyBtn"
-                onClick={addQty}
-              >
-                +
-              </button>
-            </div>
-            <button className="mt-4 w-100 addToCart" onClick={notify}>
-              <b>Añadir al carro</b>
-            </button>
-          </Col>
-        </Row>
-      </Container>
-      <ToastContainer position="top-right" autoClose={3000} />
-      <Footer />
-    </>
+            </Col>
+          </Row>
+        </Container>
+        <ToastContainer position="top-right" autoClose={3000} />
+        <Footer />
+      </>
+    )
   );
 }
