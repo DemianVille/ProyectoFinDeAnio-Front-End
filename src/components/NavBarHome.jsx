9bcf799ea39401efdc702dd36d73f2ef28a7102a
Sofía Rovati <sofia.b.rovati.f@gmail.com>
2024-06-25 20:58:40 -0300

    asx

--- src/components/NavBarHome.jsx
+++ src/components/NavBarHome.jsx
@@ -98,17 +98,18 @@ export default function NavBarHome() {
                   </div>
                 ))}
               </Nav>
+
               <div>
                 <Nav>
-                  <div className="d-flex justify-content-center align-items-center mx-4 move ">
+                  <div className="d-flex justify-content-center align-items-center">
                     <Link
-                      className="nav-link mx-1 active text-center productCard"
+                      className="nav-link mx-1 active text-center"
                       to={"/iniciar-sesion"}
                     >
-                      Iniciar
+                      Iniciar sesión
                     </Link>
                     <Link
-                      className="nav-link mx-1 active text-center productCard e"
+                      className="nav-link mx-1 active text-center"
                       to={"/registrarse"}
                     >
                       Registrarse
@@ -125,7 +126,7 @@ export default function NavBarHome() {
                       <input
                         type="text"
                         placeholder="Buscar"
-                        className="inputNav w-100 "
+                        className="inputNav w-100"
                         aria-label="Search"
                       />
                       <button
@@ -138,49 +139,10 @@ export default function NavBarHome() {
                     </Form>
                   </div>
                   <Nav>
-                    <div className="d-flex justify-content-center align-items-center">
-                      <Link
-                        className="nav-link mx-1 active text-center"
-                        to={"/iniciar-sesion"}
-                      >
-                        Iniciar sesión
-                      </Link>
-                      <Link
-                        className="nav-link mx-1 active text-center"
-                        to={"/registrarse"}
-                      >
-                        Registrarse
-                      </Link>
-                    </div>
+                    <Link className="nav-link pb-2" to={"/carrito"}>
+                      <i className="bi bi-bag-fill navIcon"></i>
+                    </Link>
                   </Nav>
-                  <div className="d-flex align-items-center">
-                    <div className="w-100 me-2">
-                      <Form
-                        className="d-flex"
-                        role="search"
-                        onSubmit={(event) => event.preventDefault()}
-                      >
-                        <input
-                          type="text"
-                          placeholder="Buscar"
-                          className="inputNav w-100"
-                          aria-label="Search"
-                        />
-                        <button
-                          className="buttonNav"
-                          type="submit"
-                          onClick={notify}
-                        >
-                          <i className="bi bi-search"></i>
-                        </button>
-                      </Form>
-                    </div>
-                    <Nav>
-                      <Link className="nav-link pb-2" to={"/carrito"}>
-                        <i className="bi bi-bag-fill navIcon"></i>
-                      </Link>
-                    </Nav>
-                  </div>
                 </div>
               </div>
             </Navbar.Collapse>
